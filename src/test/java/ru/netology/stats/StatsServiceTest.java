5e9f3b40afbb4db1ec79a2a0731bc950738508dd
Alexey 1981 <maximenko.av@rambler.ru>
2023-04-30 11:05:30 +0300

    change test2 in new-branch

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -22,7 +22,7 @@ public class StatsServiceTest {
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long actual = service.averageSumma(sales);
         long expected = 15;
-//        System.out.println(actual);
+        System.out.println("Здесь я вывел актуальные значения " + actual);
         Assertions.assertEquals(expected, actual);
 
     }
